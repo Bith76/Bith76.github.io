2c89f0d9709cd0f56040690742c0c6ad6259f0f4
Bith <Bith@Mereis-MacBook-Pro.local>
2019-04-05 17:38:03 +0200

    .

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
+<!DOCTYPE html>
 <html>
 <head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <title>Bith's github page</title>
 </head>
 <body>
-
+<h2>Welcome!</h2>
 <div>
     Hello World! :P
 </div>
